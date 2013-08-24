6b7f49a4027f4b14a93c86ce10876200f8b5b4f5
Joe Tricarico <jtricarico@gmail.com>
2013-08-24 16:36:51 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="python-td",
+    version="0.1",
+    description="Obtain transaction data from TD Bank Business accounts",
+    author="Joseph Tricarico",
+    author_email="jtricarico@gmail.com",
+    url="http://github.com/joetric/python-td",
+    license="MIT",
+    long_description=read('README.rst'),
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Office/Business :: Financial :: Accounting',
+        'Programming Language :: Python :: 2.7',
+    ],
+    # dependency_links=[],
+    # install_requires=[], 
+    # test_suite='',
+)
